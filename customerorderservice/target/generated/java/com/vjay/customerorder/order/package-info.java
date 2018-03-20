907beb7e89b8d57140480e2554f6fab59ef5bf1c
developer <dhoniramana@gmail.com>
2018-03-20 00:58:30 -0500

    Removed Target folder

--- customerorderservice/target/generated/java/com/vjay/customerorder/order/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://customerorder.vjay.com/Order", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.vjay.customerorder.order;
